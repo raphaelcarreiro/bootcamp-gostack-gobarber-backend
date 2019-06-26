9e772e7ccf3bed67973b8e58bfca6e74a3c60782
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2019-06-26 15:52:20 -0300

    User model

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) =>
-  res.json({
-    message: 'Hello World',
-  })
-);
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Raphael M Carreiro',
+    email: 'camilacarreiro@hotmail.com',
+    password_hash: '1234567489',
+  });
+
+  res.json(user);
+});
 
 export default routes;
