9cb06c7dccc030b2a88e65376a612ac789f1c21d
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2019-06-26 17:48:17 -0300

    jwt

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,6 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+class SessionController {}
+
+export default new SessionController();
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Raphael M Carreiro',
-    email: 'camilacarreiro@hotmail.com',
-    password_hash: '1234567489',
-  });
-
-  res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
